b4de3237c187baeb358ef866c743036618c53cb6
Ulises MR <umr.user@gmail.com>
2023-05-08 19:49:02 -0600

    v1 Joystick

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace TAP_U2_JoystickLego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.tmtJoystick = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.tmrGame = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(493, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.Color.White;
+            this.btnDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDown.Font = new System.Drawing.Font("IBM Plex Serif SemiBold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(211, 194);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(76, 76);
+            this.btnDown.TabIndex = 1;
+            this.btnDown.Text = "↓";
+            this.btnDown.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDown.UseVisualStyleBackColor = false;
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.White;
+            this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUp.Font = new System.Drawing.Font("IBM Plex Serif SemiBold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.Location = new System.Drawing.Point(211, 112);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(76, 76);
+            this.btnUp.TabIndex = 2;
+            this.btnUp.Text = "↑";
+            this.btnUp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnUp.UseVisualStyleBackColor = false;
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.White;
+            this.btnRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRight.Font = new System.Drawing.Font("IBM Plex Serif SemiBold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(293, 156);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(76, 76);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "→";
+            this.btnRight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnRight.UseVisualStyleBackColor = false;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackColor = System.Drawing.Color.White;
+            this.btnLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeft.Font = new System.Drawing.Font("IBM Plex Serif SemiBold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(129, 156);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(76, 76);
+            this.btnLeft.TabIndex = 4;
+            this.btnLeft.Text = "←";
+            this.btnLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnLeft.UseVisualStyleBackColor = false;
+            // 
+            // tmtJoystick
+            // 
+            this.tmtJoystick.Tick += new System.EventHandler(this.tmtJoystick_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Joystick X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Joystick Y:";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(430, 65);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(75, 17);
+            this.lblY.TabIndex = 8;
+            this.lblY.Text = "Joystick Y:";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(430, 39);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(75, 17);
+            this.lblX.TabIndex = 7;
+            this.lblX.Text = "Joystick X:";
+            // 
+            // tmrGame
+            // 
+            this.tmrGame.Interval = 50;
+            this.tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(517, 373);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "LegoJoystick";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Timer tmtJoystick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Timer tmrGame;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+//Librerias externas de Aforge y NXTNet
+using AForge;
+using AForge.Controls;
+using NxtNet;
+
+namespace TAP_U2_JoystickLego
+{
+    public partial class Form1 : Form
+    {
+        //Variables globaes para el Joystick
+        Joystick joystick;
+        List<Joystick.DeviceInfo> devicesJoysticks 
+            = Joystick.GetAvailableDevices();
+        //Objeto blobal para el robot
+        Nxt robot = new Nxt();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Mostrar los Joysticks disponibles
+            foreach (Joystick.DeviceInfo joystick in devicesJoysticks)
+            {
+                comboBox1.Items.Add(string.Format("ID: {0}, Name: {1}...", joystick.ID, joystick.Name));
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            try
+            {
+                joystick = new Joystick(comboBox1.SelectedIndex);
+                dialog = MessageBox.Show("¿Deseas activar el NXT?","JoystickLego", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (dialog == DialogResult.Yes)
+                {
+                    tmtJoystick.Enabled = true;
+                }
+                if (dialog == DialogResult.No)
+                {
+                    tmtJoystick.Enabled = false;
+                }
+
+            }
+            catch (Exception ex)
+            {                
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void tmtJoystick_Tick(object sender, EventArgs e)
+        {
+            //Conocer el estatus del Joystick
+            Joystick.Status status = joystick.GetCurrentStatus();
+            //Cargar los valores del Joystick
+            lblX.Text = status.XAxis.ToString();
+            lblY.Text = status.YAxis.ToString();
+
+
+
+            //1. Conectarnos al NXT
+            if (status.IsButtonPressed(Joystick.Buttons.Button1))
+            {
+                //Hailitar la conexion al NXT
+                try
+                {
+                    robot.Connect("COM4");
+                    robot.PlayTone(800, 2000);
+                    MessageBox.Show("Robot conectado");
+                    tmrGame.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            if (status.IsButtonPressed(Joystick.Buttons.Button3))
+            {
+                robot.SetOutputState(MotorPort.PortA, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            if (status.IsButtonPressed(Joystick.Buttons.Button2))
+            {
+                try
+                {
+                    robot.Disconnect();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }           
+
+        }
+
+        private void tmrGame_Tick(object sender, EventArgs e)
+        {
+            Joystick.Status status = joystick.GetCurrentStatus();
+
+            #region MovimientoJoystick
+            //2. Programar los Joysticks
+            if (status.XAxis.ToString() == "-1")
+            {
+                btnLeft.BackColor = Color.LightPink;
+                robot.SetOutputState(MotorPort.PortA, 100, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 0, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            else
+            {
+                btnLeft.BackColor = Color.Transparent;
+                robot.SetOutputState(MotorPort.PortA, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            if (status.XAxis.ToString() == "1")
+            {
+                btnRight.BackColor = Color.LightPink;
+                robot.SetOutputState(MotorPort.PortA, 0, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 100, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            else
+            {                
+                btnRight.BackColor = Color.Transparent;
+                robot.SetOutputState(MotorPort.PortA, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            if (status.YAxis.ToString() == "1")
+            {
+                btnDown.BackColor = Color.LightPink;
+            }
+            else
+            {
+                btnDown.BackColor = Color.Transparent;
+            }
+            if (status.YAxis.ToString() == "-1")
+            {
+                btnUp.BackColor = Color.LightPink;
+                robot.SetOutputState(MotorPort.PortA, 100, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                robot.SetOutputState(MotorPort.PortC, 100, MotorModes.On, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+            }
+            else
+            {
+                try
+                {
+                    btnUp.BackColor = Color.Transparent;
+                    robot.SetOutputState(MotorPort.PortA, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                    robot.SetOutputState(MotorPort.PortC, 0, MotorModes.Brake, MotorRegulationMode.Speed, 100, MotorRunState.Running, 0);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            #endregion
+        }
+    }
+}
